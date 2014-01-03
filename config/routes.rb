efe66fbf4015acd70444c7e59f2443b5c13cf808
slicebo123 <slicebo123@gmail.com>
2014-01-03 01:04:12 -0800

    Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 BowlApp::Application.routes.draw do
   www_host = URI.parse(BowlApp::Application.config.www_url).host
+  api_host = URI.parse(BowlApp::Application.config.api_url).host
 
-  resources :people
+  scope constraints: lambda { |request| request.host == api_host } do
+    resources :people
 
-  resources :bowls
+    resources :bowls
+  end
 
-  # scope constraints: lambda { |request| request.host == www_host } do
-  #   match '(*path)', to: 'bowl_app#home', via: [:get]
-  # end
+  scope constraints: lambda { |request| request.host == www_host } do
+    match '(*path)', to: 'bowl_app#home', via: [:get]
+  end
 end
